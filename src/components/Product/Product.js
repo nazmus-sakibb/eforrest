7db502c7a699ac7c2888d51273c288a56536a9dc
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-24 02:27:47 +0600

    product card layout design

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Product.css';
 
-const Product = ({product}) => {
-    const {name, price, img, ratings, seller} = product; 
+const Product = ({ product }) => {
+    const { name, price, img, ratings, seller } = product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
