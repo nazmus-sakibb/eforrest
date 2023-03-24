19140384e161a09109288931ed2c433b7325b9b0
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-24 16:09:15 +0600

    calculate total price and shipping price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    console.log(cart);
+    
+    let total = 0;
+    let shipping = 0;
+    
+    for(const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.price;
+    }
+
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: </p>
-            <p>Total Shipping Charge: </p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax: </p>
             <h5>Grand Total: </h5>
         </div>
