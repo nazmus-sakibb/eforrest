0e8c8a4087014e395d791b8542f2f30a68ea7ba5
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-28 02:08:54 +0600

    react router dom setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Shop/>
+      
     </div>
   );
 }
