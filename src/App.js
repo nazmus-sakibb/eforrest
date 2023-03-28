c5a5f6d8e8d9f6c340d1fbf781c7dccb3a27fbc6
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-28 13:50:23 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
+import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 import Main from './layouts/Main';
@@ -18,12 +19,16 @@ function App() {
         {
           path: '/orders',
           element: <Orders/>
+        },
+        {
+          path: '/inventory',
+          element: <Inventory/>
+        },
+        {
+          path: '/about',
+          element: <About/>
         }
       ]
-    },
-    {
-      path: '/about',
-      element: <About/>
     }
   ]);
   return (
