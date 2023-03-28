c84a68b3452b435091ee7f0f398d499b82f194dc
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-28 12:38:47 +0600

    main and outlet component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Orders from './components/Orders/Orders';
+import Shop from './components/Shop/Shop';
+import Main from './layouts/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Shop/>
+        },
+        {
+          path: '/orders',
+          element: <Orders/>
+        }
+      ]
+    },
+    {
+      path: '/about',
+      element: <About/>
+    }
+  ]);
   return (
     <div>
-      
+      <RouterProvider router={router}/>
     </div>
   );
 }
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
-import Shop from '../components/Shop/Shop';
 
 const Main = () => {
     return (
         <div>
             <Header/>
-            <Shop/>
+            <Outlet/>
         </div>
     );
 };
