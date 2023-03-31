7cdc3b203023fd157a94210131d4b1dc5794e276
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-31 15:02:26 +0600

    display review item details

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './ReviewItem.css';
 
 const ReviewItem = ({ product }) => {
-    const { name, price, quantity } = product;
+    const { name, price, quantity, img } = product;
     return (
-        <div>
-            <h3>Review item</h3>
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-details-container">
+                <div className="review-details">
+                    <p>{name}</p>
+                    <p><small>Price: {price}$</small></p>
+                    <p><small>Quantity: {quantity}</small></p>
+                </div>
+                <div className="delete-button"></div>
+            </div>
         </div>
     );
 };
