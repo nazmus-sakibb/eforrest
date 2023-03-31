4d23b8bc3416e6f654d3f8400ecdf7107d293ba6
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-31 15:54:34 +0600

    more styles for review item card

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 const ReviewItem = ({ product }) => {
-    const { name, price, quantity, img } = product;
+    const { name, price, quantity, shipping, img } = product;
     return (
         <div className='review-item'>
             <div>
@@ -11,11 +13,12 @@ const ReviewItem = ({ product }) => {
             <div className="review-details-container">
                 <div className="review-details">
                     <p>{name}</p>
-                    <p><small>Price: {price}$</small></p>
+                    <p><small>Price: ${price}</small></p>
+                    <p><small>Shipping: ${shipping}</small></p>
                     <p><small>Quantity: {quantity}</small></p>
                 </div>
-                <div className="delete-button">
-                    <button>Delete</button>
+                <div className="delete-container">
+                    <button className='btn-delete'><FontAwesomeIcon className='delete-icon' icon={faTrashCan}/></button>
                 </div>
             </div>
         </div>
