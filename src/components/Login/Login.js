edf2b427e0d8364b1cc082df9f4124efa86c2d0d
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-07 02:20:17 +0600

    implement context api

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Login.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className='form-container'>
-            <h2 className='form-title'>This is Login</h2> 
+            <h2 className='form-title'>Login</h2>
 
             {/* login form */}
             <form>
-                
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' name='email' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id='password' name='password' required />
+                </div>
+                <input className='button-submit' type="submit" value="Login" />
             </form>
+            <p><small>New to eForrest? <Link to='/signup'>Create New Account</Link></small></p>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './SignUp.css';
 
+import { Link } from 'react-router-dom';
+
 const SignUp = () => {
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+
+            {/* login form */}
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' name='email' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id='password' name='password' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm">Confirm Password</label>
+                    <input type="password" id='confirm' name='confirm' required />
+                </div>
+                <input className='button-submit' type="submit" value="Sign Up" />
+            </form>
+            <p><small>Already have an account? <Link to='/login'>Login</Link></small></p>
         </div>
     );
 };
